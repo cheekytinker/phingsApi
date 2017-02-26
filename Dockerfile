f3c8ea090b023994528373ae6f25469da95b894c
cheekytinker <github@cheekytinker.com>
2017-02-26 22:14:07 +0000

    fix dockercloud stack file to pass environment variable for mongo connection fix mongo connection to use environment variable if present

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,7 @@ COPY package.json /usr/src/app/
 COPY . /usr/src/app
 
 RUN npm install
-RUN npm build
+# RUN npm run build
 
 
 
--- src/initialiseExternalServices.js
+++ src/initialiseExternalServices.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
 
 const mongoRepository = 'phingsApi';
-const dbConnectionUrl = `mongodb://localhost:27017/${mongoRepository}`;
+const host = process.env.PHINGSAPI_DB_HOST || 'localhost';
+const dbConnectionUrl = `mongodb://${host}:27017/${mongoRepository}`;
 
 mongoose.Promise = global.Promise;
 if (mongoose.connection.readyState !== 1) {
