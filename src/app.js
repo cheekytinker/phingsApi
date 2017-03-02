34a5dbf2b84defcb11ddd016d5dadbf4b7da0262
cheekytinker <github@cheekytinker.com>
2017-03-02 16:29:25 +0000

    Allow app to trigger api tests if deployed in environment that sets the env variable

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
+import http from 'http';
 import './initialiseExternalServices';
 import EntityInitialiser from './services/authentication/entityInitialiser';
+
 
 console.log('App1 called');
 EntityInitialiser.initialise();
@@ -17,6 +19,26 @@ const config = {
   //swaggerFile: 'app/api/swagger/swagger.yaml',
 };
 
+function triggerApiTests() {
+  const shouldRunTests = process.env.RUN_API_TESTS === true;
+  const circleToken = process.env.CIRCLE_TOKEN || '';
+  if (!shouldRunTests) {
+    console.log('skipping requesting api tests');
+    return;
+  }
+  var options = {
+    host: 'https://circleci.com',
+    port: 80,
+    path: `/api/v1.1/project/github/cheekytinker/phings-api-tests/tree/master?circle-token=${circleToken}`,
+    method: 'POST',
+  };
+  http
+    .request(options, () => {
+      console.log('triggering api tests');
+    })
+    .end();
+}
+
 SwaggerRestify.create(config, (err, swaggerRestify) => {
   if (err) { throw err; }
 
@@ -27,8 +49,5 @@ SwaggerRestify.create(config, (err, swaggerRestify) => {
   app.listen(port, () => {
     console.log(`Listening on port ${port}`);
   });
-
-  if (swaggerRestify.runner.swagger.paths['/hello']) {
-    console.log(`try this:\ncurl http://127.0.0.1:${port}/hello?name=Scott`);
-  }
+  triggerApiTests();
 });
