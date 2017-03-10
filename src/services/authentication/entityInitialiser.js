ac0714952b2fdb5914b4c255795d8c4356e90f51
cheekytinker <github@cheekytinker.com>
2017-03-10 16:45:50 +0000

    Fixed master creation to have passwordhash

--- src/services/authentication/entityInitialiser.js
+++ src/services/authentication/entityInitialiser.js
@@ -11,8 +11,8 @@ export default class EntityInitialiser {
           master.firstName = 'master';
           master.lastName = 'user';
           master.userName = 'master';
-          master.passwordHash = 'test123';
-          master.passwordSalt = 'test123';
+          master.passwordHash = 'e9af9689e7b408bf2d9e5540c8a0926889061061e28c0bdcc96e0f1a27fc9d19a426debb0a27c2057156326a911a3d05a2cff9e1cf6250768d15a0e23413e168';
+          master.passwordSalt = 'salt';
           return master
             .save();
         }
