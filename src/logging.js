4cc1281611af093b6103e5e5c8658e81ebd656dc
cheekytinker <github@cheekytinker.com>
2017-03-18 12:41:37 +0000

    Ensure log folder exists

--- src/logging.js
+++ src/logging.js
@@ -1,15 +1,22 @@
 import Logger from 'bunyan';
+import fs from 'fs';
+
+const logDir = './log';
+if (!fs.existsSync(logDir)){
+  fs.mkdirSync(logDir);
+}
 
 const log = new Logger({
   name: 'phings-api',
   streams: [
     {
       level: 'info',
-      path: './log/phings-api.log',
+      path: `${logDir}/phings-api.log`,
       type: 'rotating-file',
       period: '1d',
     },
   ],
 });
+
 
 export default log;
